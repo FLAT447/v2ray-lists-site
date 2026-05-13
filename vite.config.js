90cb421968849de2e1a8195ddb89b719f9e24d83
FLAT447 <alexey_mezentsev@list.ru>
2026-05-13 17:08:52 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [svelte()],
-    base: '/v2ray-lists-site-dev/',
+    base: '/v2ray-lists-site/',
 })
